757ad6bd7c761b1b32677d44157a82908736e1cd
AngeloRoudani <ange.rouda@gmail.com>
2023-05-16 15:56:00 +0200

    implementate resources sei controller

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('title')
+    {{'Our Comics'}}
+@endsection
+
+@section('content')
+    <div class="row">
+        
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Guest\ComicsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,4 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', );
+Route::resource('comics', ComicsController::class);
